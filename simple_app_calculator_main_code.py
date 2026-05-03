63fc00198e62c3ced63277b21cbcfdc69853b739
unknown <rc.sanoiz@gmail.com>
2026-05-04 01:40:32 +0800

    basic code and loop + menu working

--- simple_app_calculator_main_code.py
+++ simple_app_calculator_main_code.py
@@ -1,15 +1,32 @@
 # simple maangas calculator app
-
+while True:
 # show menu (add, subtract, multiply, divide)
+    print("\n[1] Addition\n[2] Subtraction\n[3] Multiplication\n[4] Division")
 
 # get user choice
+    user_choice = input("Choose (1-4): ")
 
 # ask for 2 numbers
+    first_num = float(input("Enter first number: "))
+    second_num = float(input("Enter second number: "))
 
 # perform operation
+    if user_choice == "1":
+        result = first_num + second_num
+    elif user_choice == "2":
+        result = first_num - second_num
+    elif user_choice == "3":
+        result = first_num * second_num
+    elif user_choice == "4":
+        result = first_num / second_num
 
 # print result
+    print("Result:", result)
 
 # ask if user wants to try again
+    retry = input("Try again? (y/n): ")
+    if retry != 'y':
+        print("Thank you!")
+        break
 
-# handle errors (invalid input, divide by zero)
+
