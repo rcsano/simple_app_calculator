cedaafc2ddf733a987c07811c6392fff834978d3
unknown <rc.sanoiz@gmail.com>
2026-05-04 01:16:20 +0800

    outline created

---
+++ simple_app_calculator_main_code.py
@@ -0,0 +1,15 @@
+# simple maangas calculator app
+
+# show menu (add, subtract, multiply, divide)
+
+# get user choice
+
+# ask for 2 numbers
+
+# perform operation
+
+# print result
+
+# ask if user wants to try again
+
+# handle errors (invalid input, divide by zero)
